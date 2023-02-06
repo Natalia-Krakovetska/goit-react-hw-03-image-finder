3345b79a39faed7012a48542c7851c6d068fe302
Natalia <natalakrakoveckaya@gmail.com>
2023-02-06 19:31:03 +0100

    Summary

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 import { SearchBar } from "./Searchbar/Searchbar";
 import ImageGallery from "./ImageGallery/ImageGallery";
 import { fetchImages } from "../fetchImages";
